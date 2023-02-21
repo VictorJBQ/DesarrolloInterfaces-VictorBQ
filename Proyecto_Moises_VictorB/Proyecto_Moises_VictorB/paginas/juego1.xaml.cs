bd36a4fc46e50a86fe1b50381f27256a32fab413
VictorJBQ <victorbustamante07@gmail.com>
2023-02-21 20:51:01 +0100

    AvanceProyecto
    Principales ventanas

---
+++ Proyecto_Moises_VictorB/Proyecto_Moises_VictorB/paginas/juego1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Moises_VictorB.paginas
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void MyCommands_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            // El contenido central se cambia con la nueva pantalla
+            myFrame.Navigate(new juego1play());
+        }
+        private void CloseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Volvemos hacia atrás hasta llegar al contenido inicial y resetear la página central
+            /*while (myFrame.CanGoBack)
+            {
+                myFrame.GoBack();
+            }*/
+            // Otra opción es dejar el Frame a null
+            myFrame.Content= null;
+        }
+    }
+}
